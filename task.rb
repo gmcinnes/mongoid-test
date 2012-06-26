1927c4e71f8f975fe7ff7d3a59405c4c9f6897ee
Grant McInnes <gmcinnes@marsdd.com>
2012-06-26 13:49:14 -0400

    change to #new_from_hash

--- task.rb
+++ task.rb
@@ -1,4 +1,5 @@
 require File.dirname(__FILE__) + '/boot'
 
 resource = Resource.new
-resource.marc_document = MarcDocument.new(:a => 'b', :b => 'c')
+resource.marc_document = MarcDocument.new_from_hash(:a => 'b', :b => 'c')
+resource.save
